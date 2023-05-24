df7918b7ea4773d3c1a2ede7cb53dffda6e0ea78
Mikhail <misha.shabalin@mail.ru>
2023-05-24 17:33:02 +0300

    create filter for class ProductViewSet in views.py

--- logistic/views.py
+++ logistic/views.py
@@ -1,5 +1,6 @@
-from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
@@ -8,6 +9,8 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
